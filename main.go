0a6544f14a5782675230d91d39e5dfbff4ae7d45
Alex Talon <zyxancf@gmail.com>
2018-03-21 15:32:30 +0100

    refactor

--- main.go
+++ main.go
@@ -6,23 +6,21 @@ import (
 	"io"
 	"log"
 	"os"
-	"strings"
-	"sync"
 
 	"github.com/araddon/dateparse"
 	"github.com/lib/pq"
 )
 
-var (
-	file, _    = os.Open("final promocodes.csv")
-	strBuilder strings.Builder
-	wg         sync.WaitGroup
-	queries    string
-	counter    int64
-	id         = 0
-)
+// TODO
+// get biggest                                   [ID] and work with the offset
+// insert limits for coupons with ID greater than ^
 
 func main() {
+	file, err := os.Open("vuclip_codes.csv")
+	if err != nil {
+		log.Panic(err)
+	}
+
 	conn, err := sql.Open("postgres", "user=alextalon dbname=vuclip host=localhost port=5432 sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
@@ -44,22 +42,23 @@ func main() {
 		if err == io.EOF {
 			break
 		}
-		startdate, err := dateparse.ParseAny(record[1])
+
 		value := record[0]
-		expirydate, err := dateparse.ParseAny(record[3])
-		campaignid := record[2]
-		if err != nil {
-			log.Panic(err)
-		}
+		startdate, err := dateparse.ParseAny(record[1])
+		expirydate, err := dateparse.ParseAny(record[2])
+		campaignid := record[3]
+
 		_, err = stmt.Exec(startdate, value, expirydate, campaignid, 1, 4)
 		if err != nil {
 			log.Panic(err)
 		}
 	}
+
 	err = stmt.Close()
 	if err != nil {
 		log.Panic(err)
 	}
+
 	err = txn.Commit()
 	if err != nil {
 		txn.Rollback()
